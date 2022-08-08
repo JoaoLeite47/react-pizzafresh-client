b8be3a5fd50bb3c882e2d3fe218212fd1b6519d2
JoaoLeite47 <StalkerJJ@outlook.com>
2022-08-08 18:46:50 -0300

    feat<all> routes for components has creted

---
+++ src/router.tsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+import { RoutePath } from "types/routes";
+import Home from "pages/Home/index"; // not created yet
+
+const Router = () => {
+  return (
+    <Routes></Routes>
+      <Route path={RoutePath.HOME} element={<Home />} />
+    </Routes>
+  );
+};
+
+export default Router;
