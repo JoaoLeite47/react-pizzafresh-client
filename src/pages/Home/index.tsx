dcd8a2d35ae98206f75020af8c9bee6538f360c7
JoaoLeite47 <StalkerJJ@outlook.com>
2022-08-09 11:19:04 -0300

    feat<Home,Settings> change of both has worked

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,6 @@
 import Menu from "components/Menu";
-import { ReactComponent as Search } from "assets/icons/search.svg";
 import { DateTime } from "luxon";
+import { ReactComponent as Search } from "assets/icons/search.svg";
 import * as S from "./style";
 import { RoutePath } from "types/routes";
 import { navigationItems } from "data/navigation";
@@ -9,16 +9,24 @@ import ProductItem from "components/ProductItem";
 import OrderDetails from "components/OrderDetails";
 import Overlay from "components/Overlay";
 import CheckoutSection from "components/CheckoutSection";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
   const dateDescription = DateTime.now().toLocaleString({
     ...DateTime.DATE_SHORT,
     weekday: "long",
   });
+  const navigate = useNavigate();
+  const handleNavigation = (path: RoutePath) => navigate(path);
 
   return (
     <S.Home>
-      <Menu active={RoutePath.HOME} navItems={navigationItems} />
+      <Menu
+        active={RoutePath.HOME}
+        navItems={navigationItems}
+        onNavigate={handleNavigation}
+        onLogout={() => navigate(RoutePath.LOGIN)}
+      />
       <S.HomeContent>
         <header>
           <S.HomeHeaderDetails>
@@ -48,9 +56,9 @@ const Home = () => {
       <aside>
         <OrderDetails />
       </aside>
-      <Overlay>
-        <CheckoutSection />
-      </Overlay>
+      {/* <Overlay>
+                <CheckoutSection />
+            </Overlay> */}
     </S.Home>
   );
 };
